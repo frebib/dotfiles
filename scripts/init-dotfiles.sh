7118d6d63e176aab3ded946515280a30a4b39ec2
Joseph Groocock <frebib@gmail.com>
2016-03-31 18:35:15 +0100

    Link some dotfiles to /root too

--- scripts/init-dotfiles.sh
+++ scripts/init-dotfiles.sh
@@ -8,6 +8,7 @@ files=`ls -A $DOTFILES | awk '/^\..*/ && !/\.config$/ && !/\.git$/'`
 homepath=`echo $DOTFILES | sed "s|$HOME/||g"`
 conffiles=`ls -A "$DOTFILES/.config"`
 confpath=`echo $homepath | sed "s|.config/||g"`
+userhome=$HOME
 
 for file in $files; do
     ln -sfv $homepath/$file .
@@ -20,3 +21,17 @@ for file in $conffiles; do
     ln -sfv $confpath/.config/$file .
 done
 
+sudo -s << EOF
+cd ~
+echo -n "cd "
+pwd
+
+ln -sfv $userhome/.vimrc .
+ln -sfv $userhome/.vim/ .
+ln -sfv $userhome/.gtkrc-2.0 .
+
+mkdir -p .config && cd .config/
+echo -n "cd "
+pwd
+ln -sfv $userhome/.config/gtk-3.0 .
+EOF
