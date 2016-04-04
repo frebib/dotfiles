5c986d3203914150e14604b9938e44d782aff59f
Joseph Groocock <frebib@gmail.com>
2016-04-04 04:04:20 +0100

    powermenu: Add a 'Reboot to Windows' option

--- .config/i3/powermenu.sh
+++ .config/i3/powermenu.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
  
-option=$(echo "         lock|       logout|        sleep|    shutdown|       restart" | rofi -sep "|" -dmenu -i -p "" -auto-select -hide-scrollbar -monitor 0 -width 8 -location 3 | xargs)
+option=$(echo "         lock|       logout|        sleep|    shutdown|       restart|     windows" | rofi -sep "|" -dmenu -i -p "" -auto-select -hide-scrollbar -monitor 0 -width 8 -location 3 | xargs)
 
 case $option in
     lock) ./lock.sh;;
     logout) i3-msg exit;;
-    restart) reboot;;
     sleep) systemctl suspend;;
     shutdown) poweroff;;
+    restart) reboot;;
+    windows)
+        windows=`efibootmgr | grep -Po -m1 'Boot\K(\d{4}).*Windows.*' | head -c4`
+        `sudo efibootmgr -n $windows`
+        reboot;;
 esac
 
 exit 0
