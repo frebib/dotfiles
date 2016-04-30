23486ae64102276a4f54bc46545d8241abe9394d
Joseph Groocock <frebib@gmail.com>
2016-04-30 14:06:10 +0100

    Add poweroff alias option to power menu

--- .config/i3/powermenu.sh
+++ .config/i3/powermenu.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
  
-option=$(echo "         lock|       logout|        sleep|    shutdown|       restart|     windows" | rofi -sep "|" -dmenu -i -p "" -auto-select -hide-scrollbar -monitor 0 -width 8 -location 3 | xargs)
+option=$(echo "         lock|       logout|        sleep|    power off|    shutdown|       restart|     windows" | rofi -sep "|" -dmenu -i -p "" -auto-select -hide-scrollbar -monitor 0 -width 8 -location 3 | xargs)
 
 case $option in
     lock) ./lock.sh;;
     logout) i3-msg exit;;
     sleep) systemctl suspend;;
-    shutdown) poweroff;;
-    restart) reboot;;
+    shutdown|'power off') systemctl poweroff;;
+    restart) systemctl reboot;;
     windows)
         windows=`efibootmgr | grep -Po -m1 'Boot\K(\d{4}).*Windows.*' | head -c4`
         `sudo efibootmgr -n $windows`
