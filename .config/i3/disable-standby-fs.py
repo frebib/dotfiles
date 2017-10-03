3915bfa6bb514f44a58bae6753707f1a3d9e8166
Joe Groocock <frebib@gmail.com>
2017-10-03 11:35:02 +0100

    i3/config: fix fullscreen lock disabling

--- .config/i3/disable-standby-fs.py
+++ .config/i3/disable-standby-fs.py
@@ -16,16 +16,16 @@ args = parser.parse_args()
 
 def on_fullscreen_mode(i3, e):
     if e.container.props.fullscreen_mode:
-        call(['xset', 's', 'off'])
-        call(['xset', '-dpms'])
+        call(['xautolock', '-disable'])
+        print("disabling autolock")
     else:
-        call(['xset', 's', 'on'])
-        call(['xset', '+dpms'])
+        call(['xautolock', '-enable'])
+        print("enabling autolock")
 
 def on_window_close(i3, e):
     if e.container.props.fullscreen_mode:
-        call(['xset', 's', 'on'])
-        call(['xset', '+dpms'])
+        call(['xautolock', '-enable'])
+        print("enabling autolock")
 
 i3.on('window::fullscreen_mode', on_fullscreen_mode)
 i3.on('window::close', on_window_close)
