83f8566faf9c0f803b3d3b32578ac802dd247e3f
Joe Groocock <me@frebib.net>
2018-11-28 20:03:00 -0800

    i3: widen powermenu to account for padding

--- .config/i3/powermenu.sh
+++ .config/i3/powermenu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
  
-option=$(echo "lock|logout|sleep|hibernate|power off|reboot|windows" | rofi -sep "|" -dmenu -i -p "" -auto-select -hide-scrollbar -monitor 0 -width 8 -lines 7 -location 3 -tokenize false -matching normal | xargs)
+option=$(echo "lock|logout|sleep|hibernate|power off|reboot|windows" | rofi -sep "|" -dmenu -i -p "" -auto-select -hide-scrollbar -monitor 0 -width 10 -lines 7 -location 3 -tokenize false -matching normal | xargs)
 
 case $option in
     lock) dm-tool switch-to-greeter;;
