5547ec844c4f9c1dbd374a37f641d9c504d3829d
Joe Groocock <me@frebib.net>
2020-03-22 12:44:57 +0000

    i3/powermenu: add pretty icons

--- .config/i3/powermenu.sh
+++ .config/i3/powermenu.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
- 
-option=$(echo "lock|logout|sleep|hibernate|power off|reboot|windows" | rofi -sep "|" -dmenu -i -p "" -auto-select -hide-scrollbar -monitor 0 -width 10 -lines 7 -location 3 -tokenize false -matching normal | xargs)
+
+option="$(echo "  lock|  logout|  sleep|  hibernate|  power off|  reboot|  windows" | \
+    rofi -sep "|" -dmenu -i -p "" -auto-select -hide-scrollbar -monitor 0 -width 10 -lines 7 -location 3 -tokenize false -matching normal | cut -d' ' -f2- | xargs)"
 
 case $option in
     lock) dm-tool switch-to-greeter;;
