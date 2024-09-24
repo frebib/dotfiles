068889b982a0170219128e8fdd6296a3947021a0
Joe Groocock <me@frebib.net>
2024-09-24 00:16:42 +0000

    i3/powermenu: Add 'firmware' reboot option
    Reboots into UEFI firmware interface.
    
    Signed-off-by: Joe Groocock <me@frebib.net>

--- i3/powermenu.sh
+++ i3/powermenu.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-option="$(echo "  lock|  logout|  sleep|  hibernate|  power off|  reboot|  windows" | \
+option="$(echo "  lock|  logout|  sleep|  hibernate|  power off|  reboot|  firmware|  windows" | \
     rofi -sep "|" -dmenu -i -p "" -auto-select -hide-scrollbar -monitor 0 -width 10 -lines 7 -location 3 -tokenize false -matching normal | cut -d' ' -f2- | xargs)"
 
 case $option in
@@ -10,8 +10,9 @@ case $option in
     hibernate) systemctl hibernate;;
     power\ off) systemctl poweroff;;
     reboot) systemctl reboot;;
+    firmware) systemctl reboot --firmware-setup;;
     windows)
-        windows=`efibootmgr | grep -Po -m1 'Boot\K(\d{4}).*Windows.*' | head -c4`
+        windows="$(efibootmgr | grep -Po -m1 'Boot\K(\d{4}).*Windows.*' | head -c4)"
         sudo efibootmgr -n $windows
-        reboot;;
+        systemctl reboot;;
 esac
